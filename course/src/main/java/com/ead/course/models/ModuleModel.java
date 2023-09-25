6aaef32449d0deed792fea69493eeb598cbd3497
NormandoJefferson <normandojefferson@hotmail.com>
2023-09-25 08:42:00 -0300

    Create course CRUD

--- course/src/main/java/com/ead/course/models/ModuleModel.java
+++ course/src/main/java/com/ead/course/models/ModuleModel.java
@@ -33,7 +33,7 @@ public class ModuleModel implements Serializable {
     private LocalDateTime creationDate;
 
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
     private CourseModel course;
 
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
